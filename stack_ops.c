7b430877e59f67bfd99958801daaf4f1504704bf
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-08-24 13:59:09 +0300

    Create stack_ops.c

---
+++ stack_ops.c
@@ -0,0 +1,17 @@
+/**
+ * free_nodes - Frees nodes in the stack.
+ */
+void free_nodes(void)
+{
+	stack_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
