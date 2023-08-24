4050dd9687eb4f7f78ecd814b12b54903af81d06
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-08-24 19:57:29 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -45,7 +45,7 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-typedef void (*opcode_func)(stack_t **, unsigned int);
+typedef void (*opcode_function)(stack_t **, unsigned int);
 
 /* Data structure operations*/
 stack_t *create_node(int data);
