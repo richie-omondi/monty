4252899023405bc15329be8024194a33207bf213
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-08-24 22:20:37 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -50,12 +50,12 @@ typedef void (*opcode_function)(stack_t **, unsigned int);
 /* Data structure operations*/
 stack_t *create_node(int data);
 void free_node(void);
-void call_function(opcode_func func, char *op_code, char *num, int line_no, int data_struct);
+void call_function(opcode_func func, char *op_code, char *num, int line_number, int data_struct);
 void find_function(char *opcode, char *opcode_argument, int line_number, int data_structure);
 int tokenize(char *input, int line_number, int data_structure);
 void open_file(char *pathname);
 void read_input_in_file(FILE *fd);
-void add_to_queue(stack_t **new_node, unsigned int line_no);
+void add_to_queue(stack_t **new_node, unsigned int line_number);
 void push_to_stack(stack_t **new_node, unsigned int line_number);
 
 /** Print errors **/
