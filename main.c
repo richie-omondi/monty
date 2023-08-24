c948d56c26306692da3c75f7855873d02ad21f92
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-08-24 22:16:52 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+stack_t *head = NULL;
+
+/**
+ * main - entry point of the program
+ * @ac: argument count
+ * @av: list of arguments
+ * Return: always 0
+ */
+
+int main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	open_file(av[1]);
+	free_node();
+	return (0);
+}
