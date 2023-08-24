7919917eb16e64e4df8983e6933f8c1b9c282903
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-08-24 19:09:54 +0300

    Update monty.h with the struct that contains the opcode and ts function

--- monty.h
+++ monty.h
@@ -28,6 +28,21 @@ typedef struct stack_s
 } stack_t;
 
 extern stack_t *head;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
 typedef void (*opcode_func)(stack_t **, unsigned int);
 
 /* Stack operations*/
