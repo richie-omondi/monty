046f8fc297bb40c729b19a9fa41766bd66864f27
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-08-24 23:29:35 +0300

    Update opcodes.c with a function to print all the values in a stack

--- opcodes.c
+++ opcodes.c
@@ -23,3 +23,23 @@ void push_to_stack(stack_t **new_node, unsigned int line_number)
 	head->next = temp;
 	temp->prev = head;
 }
+
+/**
+ * print_stack - Prints all the values in a stack.
+ * @stack: Pointer to a pointer pointing to the top node of the stack.
+ * @line_number: line number of  the opcode.
+ */
+void print_stack(stack_t **stack, __attribute__((unused))unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (stack == NULL)
+		exit(EXIT_FAILURE);
+	temp = *stack;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
+
