add9b54341c566b9a0b8fc41c843300ba7d9c25c
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-08-25 04:55:46 +0300

    Update opcodes_2.c

--- opcodes_2.c
+++ opcodes_2.c
@@ -2,7 +2,7 @@
 
 /**
  * add - Adds the top two elements of the stack.
- * @stack: Double pointer pointing to top node of the stack.
+ * @stack: Double pointer pointing to the top node of the stack.
  * @line_number: Integer representing the line number of the opcode.
  */
 void add(stack_t **stack, unsigned int line_number)
@@ -29,3 +29,26 @@ void nop(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	(void)line_number;
 }
+
+/**
+ * sub_nodes - Subtracts the top element of the stack from the
+ * second top element of the stack.
+ * @stack: Double pointer pointing to the top node of the stack.
+ * @line_number: Integer representing the line number of the opcode.
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	int difference;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+
+		print_other_errors(8, line_number, "sub");
+
+
+	(*stack) = (*stack)->next;
+	difference = (*stack)->n - (*stack)->prev->n;
+	(*stack)->n = difference;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+
