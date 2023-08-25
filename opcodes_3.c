a1068bdf82d0394c5876d5464072e3c5b1afe389
Richard Orido <69873039+richie-omondi@users.noreply.github.com>
2023-08-25 05:37:24 +0300

    Create opcodes_3.c to add modulus functionality

---
+++ opcodes_3.c
@@ -0,0 +1,23 @@
+/**
+ * mod - computes the rest of the division of the second top element
+ * of the stack by the top element of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
+ */
+void mod(stack_t **stack, unsigned int line_number)
+{
+	int modulus;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+
+		print_other_errors(8, line_number, "mod");
+
+
+	if ((*stack)->n == 0)
+		print_other_errors(9, line_number);
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n % (*stack)->prev->n;
+	(*stack)->n = modulus;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
